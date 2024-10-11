dafdfcfe115fa31842591d8b165ea6983b50e589
Dvis <gokhaledurgesh4@gmail.com>
2024-10-11 13:02:35 +0530

    Auto Fixes

---
+++ ERAVIBES/__init__.py
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from ERAVIBES.core.bot import ERABot
+from ERAVIBES.core.dir import dirr
+from ERAVIBES.core.git import git
+from ERAVIBES.core.userbot import Userbot
+from ERAVIBES.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Bot Client
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+app = ERABot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+HELPABLE = {}
