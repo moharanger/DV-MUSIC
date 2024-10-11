553ced14c1a19414c7c47152f992192ffbde12ae
Dvis <gokhaledurgesh4@gmail.com>
2024-10-11 22:09:53 +0530

    Auto Fixes

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/",
+    "https://github.com/IamDvis/DV-EXTRAPLUGIN",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
