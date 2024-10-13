72c6a1aa84185a90097e2c6a7f161c990f4e4791
Dvis <gokhaledurgesh4@gmail.com>
2024-10-13 19:56:12 +0530

    Auto Fixes

--- ERAVIBES/plugins/sudo/heroku.py
+++ ERAVIBES/plugins/sudo/heroku.py
@@ -318,7 +318,7 @@ async def update_(client, message, _):
         exit()
 
 
-@app.on_message(filters.command(["restart"]) & SUDOERS)
+@app.on_message(filters.command(["restart", "estart"], prefixes=["/", "!", ".", "R", "r"]) & SUDOERS)
 async def restart_(_, message):
     response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
     ac_chats = await get_active_chats()
