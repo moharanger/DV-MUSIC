b5312e2ea8f370c2eb9122ca4829242ad4c1a220
Dvis <gokhaledurgesh4@gmail.com>
2024-10-13 19:58:02 +0530

    Auto Fixes

--- ERAVIBES/utils/inline/play.py
+++ ERAVIBES/utils/inline/play.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
 #
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
 # and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
 #
 # All rights reserved.
 #
