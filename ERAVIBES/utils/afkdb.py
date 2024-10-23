f7cf6bc381ab956c1e39fde1011e1787d0489827
Dvis <gokhaledurgesh4@gmail.com>
2024-10-23 19:07:14 +0530

    Auto Fixes

---
+++ ERAVIBES/utils/afkdb.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from ERAVIBES.core.mongo import mongodb
+
+HEHE = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"
+LOGGERS = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"
+afkdb = mongodb.afk
+
+
+async def is_afk(user_id: int) -> bool:
+    user = await afkdb.find_one({"user_id": user_id})
+    if not user:
+        return False, {}
+    return True, user["reason"]
+
+
+async def add_afk(user_id: int, mode):
+    await afkdb.update_one(
+        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+    )
+
+
+async def remove_afk(user_id: int):
+    user = await afkdb.find_one({"user_id": user_id})
+    if user:
+        return await afkdb.delete_one({"user_id": user_id})
+
+
+async def get_afk_users() -> list:
+    users = afkdb.find({"user_id": {"$gt": 0}})
+    if not users:
+        return []
+    users_list = []
+    for user in await users.to_list(length=1000000000):
+        users_list.append(user)
+    return users_list
